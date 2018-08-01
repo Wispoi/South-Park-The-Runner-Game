420ce5bf8e3350e300d3a24e26222347f5857fa2
Wispoi <alexastap95@gmail.com>
2018-08-01 16:21:43 +0300

    New files

---
+++ src/game/config.js
@@ -0,0 +1,15 @@
+game.config = {
+    name: 'South Park: The Runner Game',
+
+    system: {
+        width: 768,
+        height: 1024,
+        startScene: 'Title',
+        scale: true,
+        center: true,
+        resize: false
+    },
+
+    mobile: {
+    }
+};
